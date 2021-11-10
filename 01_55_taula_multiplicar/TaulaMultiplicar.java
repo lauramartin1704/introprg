59ccfdfd6a87801d7d7090235df43158a5fe3602
Laura <cf20laura.martin@iesjoandaustria.org>
2021-11-10 15:15:04 +0100

    Taula de multiplicar

--- 01_55_taula_multiplicar/TaulaMultiplicar.java
+++ 01_55_taula_multiplicar/TaulaMultiplicar.java
@@ -1,29 +1,18 @@
 /*
 * Programa que retorna la taula de multiplicar demanada.
 */
-public class TaulaMultiplicar{
-    public static void main(String[] args){
-    
-    int result = 0;
-    int primerX = Integer.parseInt(args[0]);
-    int segonX = Integer.parseInt(args[1]);
-    int primerM = Integer.parseInt(args[2]); 
-    int segonM = Integer.parseInt(args[3]); 
-    
-      
-         
-         for(int fila = primerX; fila <= segonX; fila++){ //Agafa el primer operant i va sumant-lo fins q (0)
-                                                          //arriba al últim operant (1)
-            for(int num = primerM; num <= segonM; num++){ //Aixó agafa el primer múltiplicant (2) i suma fins arribar a l'últim (3)
-                 
-                if(num == primerM){ //* Només si ho fa si es el primer multiplicant (2)
-                result = fila*primerM;
-                System.out.println(fila + " x " + num + " = "+result); 
-                } else if(num == segonM) { //* Només si ho fa si es el segon multiplicant (3)
-                result = fila*segonM;
-                System.out.println(fila + " x " + num + " = "+result); 
-                }
+public class TaulaMultiplicar {
+    public static void main(String[] args) {
+    int primer = Integer.parseInt(args[0]); 
+    int segon = Integer.parseInt(args[1]); 
+    int tercer = Integer.parseInt(args[2]); 
+    int quart = Integer.parseInt(args[3]); 
+    int multiplicacio;
+        for (int i=primer; i<=segon; i++){
+            for (int j=tercer; j<=quart; j++){
+            multiplicacio = i * j; 
+            System.out.println( i + " x " + j + " = " + multiplicacio);
             }
-         }       
-   } 
+        }
+    }
 }  
