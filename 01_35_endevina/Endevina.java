ea4f979ea61ceb594d21f08a8da315a5c6fa7838
Laura <cf20laura.martin@iesjoandaustria.org>
2021-11-10 18:26:02 +0100

    Programa que pensa un numero i el usuari l'ha d'endevinar

--- 01_35_endevina/Endevina.java
+++ 01_35_endevina/Endevina.java
@@ -6,26 +6,26 @@ public class Endevina{
         
         boolean encertat = false;
         
-        System.out.println("Nombre?");
+        System.out.println("Introdueix un nombre entre el 1 i el 100");
         int num = Integer.parseInt(Entrada.readLine());
         
         while (!encertat){ //*Mentres sigui fals 
             if (num >= 1 && num <= 41){
                 System.out.println("Massa petit"); //* Print massa petit entre 0 - 41
-                System.out.println("Nombre?");
+                System.out.println("Introdueix un nombre entre el 1 i el 100");
                 num = Integer.parseInt(Entrada.readLine()); 
             } else if (num >= 43 && num <= 100) {
                 System.out.println("Massa gran"); //* Print massa gran entre 43 - 100
-                System.out.println("Nombre?");
+                System.out.println("Introdueix un nombre entre el 1 i el 100");
                 num = Integer.parseInt(Entrada.readLine());
             } else if (num == 42) { 
                 encertat = true; //*Li dic que aixó es veritat
             } else {
                 System.out.println("Fora de rang");  //*La resta esta fora de rang
-                System.out.println("Nombre?");
+                System.out.println("Introdueix un nombre entre el 1 i el 100");
                 num = Integer.parseInt(Entrada.readLine()); 
             }
         }
-        System.out.println("Encertat!");
+        System.out.println("Has encertat!");
    }
 }
