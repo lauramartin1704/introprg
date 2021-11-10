bb34a8ceaf362ed10a7788852c9f122b059f93a0
Laura <cf20laura.martin@iesjoandaustria.org>
2021-11-10 18:48:21 +0100

    Programa que pensa un numero i el usuari l'ha d'endevinar

--- 01_35_endevina/Endevina.java
+++ 01_35_endevina/Endevina.java
@@ -5,41 +5,28 @@ public class Endevina{
     public static void main(String [] args){
         
         boolean encertat = false;
+        int numero =42;
         
-        System.out.println("Introdueix un nombre entre el 1 i el 100");
-        int num = Integer.parseInt(Entrada.readLine());
+        int entrada;
         
         while (!encertat){ //*Mentres sigui fals 
-            if (num >= 2 && num <= 5){
-                System.out.println("És més gran que 5"); //* Print massa petit entre 0 - 41
-                System.out.println("Introdueix un nombre entre el 1 i el 100");
-                num = Integer.parseInt(Entrada.readLine()); 
-            } else if (num >= 43 && num <= 50) {
-                System.out.println("És més petit que 50"); //* Print massa gran entre 43 - 100
-                System.out.println("Introdueix un nombre entre el 1 i el 100");
-                num = Integer.parseInt(Entrada.readLine());
-            } else if (num >= 51 && num <= 100) {
-                    System.out.println("És més petit que 56"); //* Print massa gran entre 43 - 100
-                    System.out.println("Introdueix un nombre entre el 1 i el 100");
-                    num = Integer.parseInt(Entrada.readLine());
-            } else if (num >= 6 && num <= 41) {
-                System.out.println("És més gran que 10"); //* Print massa gran entre 43 - 100
-                System.out.println("Introdueix un nombre entre el 1 i el 100");
-                num = Integer.parseInt(Entrada.readLine());
-            } else if (num == 42) { 
-                encertat = true; //*Li dic que aixó es veritat
-            } else if (num == 1) { 
-                System.out.println("És més gran que 1"); 
-                System.out.println("Introdueix un nombre entre el 1 i el 100");
-                num = Integer.parseInt(Entrada.readLine()); 
-            }else if (num <= 0) { 
+            System.out.println("Introdueix un nombre entre el 1 i el 100");
+            entrada = Integer.parseInt(Entrada.readLine()); 
+            if (numero>entrada){
+                System.out.println("És més gran que " + entrada); 
+               
+            } else if (numero<entrada) {
+                System.out.println("És més petit que "+ entrada); 
+
+            } else if (numero==entrada) {
+                encertat = true;
+            
+            }else if (entrada <= 0) { 
                 System.out.println("Com a mínim 1"); 
-                System.out.println("Introdueix un nombre entre el 1 i el 100");
-                num = Integer.parseInt(Entrada.readLine()); 
+              
             } else {
                 System.out.println("Com a màxim 100");  //*La resta esta fora de rang
-                System.out.println("Introdueix un nombre entre el 1 i el 100");
-                num = Integer.parseInt(Entrada.readLine()); 
+          
             }
         }
         System.out.println("Has encertat!");
