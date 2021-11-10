04b7f4f5e5f03eef50e976046d5236c5555c4728
Laura <cf20laura.martin@iesjoandaustria.org>
2021-11-10 18:36:57 +0100

    Programa que pensa un numero i el usuari l'ha d'endevinar

--- 01_35_endevina/Endevina.java
+++ 01_35_endevina/Endevina.java
@@ -14,10 +14,14 @@ public class Endevina{
                 System.out.println("És més gran que 5"); //* Print massa petit entre 0 - 41
                 System.out.println("Introdueix un nombre entre el 1 i el 100");
                 num = Integer.parseInt(Entrada.readLine()); 
-            } else if (num >= 43 && num <= 100) {
+            } else if (num >= 43 && num <= 50) {
                 System.out.println("És més petit que 50"); //* Print massa gran entre 43 - 100
                 System.out.println("Introdueix un nombre entre el 1 i el 100");
                 num = Integer.parseInt(Entrada.readLine());
+            } else if (num >= 51 && num <= 100) {
+                    System.out.println("És més petit que 56"); //* Print massa gran entre 43 - 100
+                    System.out.println("Introdueix un nombre entre el 1 i el 100");
+                    num = Integer.parseInt(Entrada.readLine());
             } else if (num >= 6 && num <= 41) {
                 System.out.println("És més gran que 10"); //* Print massa gran entre 43 - 100
                 System.out.println("Introdueix un nombre entre el 1 i el 100");
