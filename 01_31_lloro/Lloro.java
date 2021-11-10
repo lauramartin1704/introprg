657466363f58d3d86f3b7909219e212f5aecd289
Laura <cf20laura.martin@iesjoandaustria.org>
2021-11-10 15:26:24 +0100

    programa que repeteix paraules

--- 01_31_lloro/Lloro.java
+++ 01_31_lloro/Lloro.java
@@ -5,12 +5,12 @@ public class Lloro {
 	public static void main(String [] args) {
 	
 	
-	System.out.println("El lloro pregunta paraula:");
+	System.out.println("El lloro espera paraula:");
 	String paraula = Entrada.readLine();
 	
 	while (!(paraula.isBlank())){
 		System.out.println("El lloro diu: " + paraula);
-		System.out.println("El lloro pregunta paraula:");
+		System.out.println("El lloro espera paraula:");
 		paraula = Entrada.readLine();
 	}
 	System.out.println("Adéu");
