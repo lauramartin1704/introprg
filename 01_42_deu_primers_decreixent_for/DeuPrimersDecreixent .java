61861d21da8a2370cf6bc487b6bf8f488cc8d7bb
Laura <cf20laura.martin@iesjoandaustria.org>
2021-11-11 15:27:47 +0100

    deu primers decreixent

--- 01_42_deu_primers_decreixent_for/DeuPrimersDecreixent .java
+++ 01_42_deu_primers_decreixent_for/DeuPrimersDecreixent .java
@@ -1,7 +1,7 @@
 /*
 *Exercici del 10 al 1 amb for
 */
-public class DeuPrimersDecreixent{
+public class DeuPrimersNaturalsDecreixent{
     public static void main(String [] args){
         
         for(int num = 10; num >= 1; num--){
