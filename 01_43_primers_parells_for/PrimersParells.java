97076eace729d47afbe6203778b5057a7d289044
Laura <cf20laura.martin@iesjoandaustria.org>
2021-11-13 16:31:33 +0100

    de 2 al 10

--- 01_43_primers_parells_for/PrimersParells.java
+++ 01_43_primers_parells_for/PrimersParells.java
@@ -1,6 +1,7 @@
-//* Programa de 2 al 10 amb for (amb parells)
-
-public class DeuPrimersNaturals{
+/*
+*Programa de 2 al 10 amb for (amb parells)
+*/
+public class PrimersParells{
     public static void main(String [] args){
         
         for(int num = 2; num <= 10; num = num + 2){
