11c2e7d9788a89faf749d8db4c04bbd9519457a8
Laura <cf20laura.martin@iesjoandaustria.org>
2021-11-13 16:19:56 +0100

    del 10 al 1

---
+++ 01_38_deu_primers_decreixent/DeuPrimersNaturalsDecreixent.java
@@ -0,0 +1,14 @@
+/*
+*Programa que retorna els de el 10 al 1
+*/
+public class DeuPrimersNaturalsDecreixent{
+    public static void main(String [] args){
+        
+        int num = 10;
+        
+        while (num >= 1){
+        System.out.println(num);
+        num = num - 1;
+        }
+    }
+}
