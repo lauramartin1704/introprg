5962bb63272fb3cff1aad18e51716ba186b079c6
Laura <cf20laura.martin@iesjoandaustria.org>
2021-11-24 18:43:11 +0100

    Lloro vocal minusculas

---
+++ 01_61_acaba_lletra/LloroAcabaLletra.java
@@ -0,0 +1,25 @@
+//* Programa que repeteix paraules que comencen per vocals i acaben amb una lletra
+
+public class LloroAcabaLletra{
+    public static void main(String [] args){
+        
+        String paraula = "hola";  
+        
+        while (!(paraula.isBlank())){
+            System.out.println("El lloro pregunta paraula que finalitzi per lletra");
+            paraula = Entrada.readLine();
+            
+            if(!paraula.isBlank()){
+                char lletra = paraula.charAt(0);
+                int lon = paraula.length();
+                char fin = paraula.charAt(lon - 1);
+                
+                if (Character.isLetter(fin) == true){
+                    System.out.println("El lloro diu: " + paraula);
+                }
+            }    
+        }
+        System.out.println("Adéu");
+   }
+}
+            
