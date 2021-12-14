bd75f3281bb8a1641e7a1a35a9ebbeee4418f96b
Laura <cf20laura.martin@iesjoandaustria.org>
2021-12-14 15:16:18 +0100

    fet

--- 02_01_lloro_quadrat/LloroQuadrat.java
+++ 02_01_lloro_quadrat/LloroQuadrat.java
@@ -2,23 +2,44 @@
  * Programa que dibuixa un quadrat de X amb el costat indicat per args[0]
  * Pressuposa que args[0] conté un valor enter vàlid
  */
-
 public class LloroQuadrat {
     public static void main(String[] args) {
-        int costat = Integer.parseInt(args[0]);
-        dibuixaQuadrat(costat);
-    }
+    	
+    	String entrada = "hola";
+    	
+    	while (!entrada.isBlank()){
+    		System.out.println("El lloro espera paraula:");
+    		entrada = Entrada.readLine();
+    		if(entrada.equals("dibuixa quadrat")) {
+    		   dibuixaQuadrat();
+    		} else if (entrada.equals("dibuixa rectangle")) {
+    		   dibuixaRectangle();
+    		} else if (!entrada.isBlank()) {
+    		   System.out.println("El lloro repeteix: " + entrada);
+    		}
+    	   }
+    	   System.out.println("Adéu");
+     }
     
-    public static void dibuixaQuadrat(int costat) {
-        for (int linia=1; linia <= costat; linia++) {
-            dibuixaLinia(costat);
-            System.out.println();
+    public static void dibuixaQuadrat() {
+        for (int linia=1; linia <= 5; linia++) {
+            // dibuixa una línia
+            for (int columna=1; columna <= 5; columna++) {
+                // dibuixa un element de la línia
+                System.out.print(" X");
+            }
+            System.out.println("");   // finalitza la línia
         }
     }
-
-    public static void dibuixaLinia(int costat) {
-        for (int columna=1; columna <= costat; columna++) {
-            System.out.print(" X");
+    
+    public static void dibuixaRectangle() {
+        for (int linia=1; linia <= 5; linia++) {
+            // dibuixa una línia
+            for (int columna=1; columna <= 10; columna++) {
+                // dibuixa un element de la línia
+                System.out.print(" X");
+            }
+            System.out.println("");   // finalitza la línia
         }
     }
 }
