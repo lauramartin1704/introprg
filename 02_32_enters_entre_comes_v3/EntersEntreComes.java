5a62040419db71838e4620e0bccd78e85211a36a
Laura <cf20laura.martin@iesjoandaustria.org>
2022-01-18 20:27:45 +0100

    fet l'exercici 32

--- 02_32_enters_entre_comes_v3/EntersEntreComes.java
+++ 02_32_enters_entre_comes_v3/EntersEntreComes.java
@@ -1,65 +1,30 @@
-// Función Numeros entre comas
-// Pide la longitud de la lista, los valores y el separador.
-
+/* 
+*Programa numero demanat y quants numeros entre comes amb Array
+*/
 public class EntersEntreComes {
 
     public static void main(String [] args){
-    
-        String aux;
-        //Demana la quantitat. 
         
         System.out.println("Quants?");
-        String text = Entrada.readLine();
-        int quantitat = numValid(text);
-        if (quantitat < 0){
-            System.out.println("Res a fer"); return;
+        int quantitat = Integer.parseInt(Entrada.readLine());
+        
+        if(quantitat < 0){
+            System.out.println("Res a fer");
+            return;
         }
-
-        
-        // Demana el separador
-        
-        System.out.println("Separador?");   
-        String separador = Entrada.readLine(); 
-        if(separador.isEmpty()){
-            separador = ",";
-        } 
-        char caracter = separador.charAt(0);
-
-
-        // Demana els valors
         
         int[] numeros;
-        numeros = new int[quantitat];
-        for(int i = 0; i < quantitat; i++){  
-            System.out.println("Valor "+ (i+1) + "?");
-            aux = Entrada.readLine(); 
-            int num = numValid(aux);        //verifica que sigui un numero
+        numeros = new int[quantitat + 1];
+        for(int i = 1; i < quantitat + 1; i++){  //Demana els valors
+            System.out.println("Valor "+ i + "?");
+            int num = Integer.parseInt(Entrada.readLine());
             numeros[i] = num; 
            
         }
-        
-        //Escriu els números
-        String resposta = UtilString.entreComes(numeros, caracter);
-        System.out.println(resposta);
+        System.out.print(numeros[1]);
+        for (int i = 2; i < numeros.length; i++) { //Print els valors entre comes
+            System.out.print(", " + numeros[i]);
+        }
         System.out.println();
-            
-    }
-    public static int numValid(String text){
-    
-         boolean numValid = false;
-           
-         while(!numValid){  //Mentres no sigui numero, va demanant. 
-
-        
-            numValid = UtilString.esEnter(text);
-        
-            if(!numValid){
-                System.out.println("Per favor, un valor enter");
-                text = Entrada.readLine();
-            }
-         }
-         int numeroEnter = Integer.parseInt(text);
-         
-         return numeroEnter;
     }
 }
