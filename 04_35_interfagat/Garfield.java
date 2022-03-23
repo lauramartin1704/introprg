847b81289571362b502d1a6cfe4752df84dda884
Laura <cf20laura.martin@iesjoandaustria.org>
2022-03-24 00:19:18 +0100

    fet

--- 04_35_interfagat/Garfield.java
+++ 04_35_interfagat/Garfield.java
@@ -11,6 +11,6 @@ public class Garfield extends Gat implements AnimalDeCompanyia{
     }
     
     public String deixatEstimar() {
-         return "em deixo estimar una mica";
+         return "em deixo estimar, però només una mica";
     }
 }
