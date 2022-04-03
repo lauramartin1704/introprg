6a0b14bd2f1c6271f4e76322af67c5ed850f1566
Laura <cf20laura.martin@iesjoandaustria.org>
2022-04-03 22:39:55 +0200

    fet

--- 04_13_ascensor_protegit/Ascensor.java
+++ 04_13_ascensor_protegit/Ascensor.java
@@ -13,7 +13,7 @@ public class Ascensor {
 		if (nouPis < 11 && nouPis > -2) {
 			pis = nouPis;
 		} else{
-			return;
+			break;
 		}
     }
     public String getMoviment() {
@@ -23,7 +23,7 @@ public class Ascensor {
 		if (nouMoviment.equals("pujant") || nouMoviment.equals("baixant") || nouMoviment.equals("aturat")) {
 			moviment = nouMoviment;
 		}else{
-			return;
+			break;
 		}	
 	}
 		
