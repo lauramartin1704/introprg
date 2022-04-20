1a68244aed64c53cf7fa7f16994afef4aeac2c84
Laura <cf20laura.martin@iesjoandaustria.org>
2022-04-20 15:18:32 +0200

    fet

--- 05_02_botiga/UsaBotiga.java
+++ 05_02_botiga/UsaBotiga.java
@@ -1,22 +1,18 @@
-/*
- * Aquesta classe fa una comprovació ràpida del funcionament de la classe
- * Botiga
- */
 public class UsaBotiga {
     public static void main(String[] args) {
-        final int QUANTS_VINS = 3;
-
         System.out.println("Creem uns quants vins");
-        Vi[] vins = new Vi[QUANTS_VINS];
-        for (int i=0; i<QUANTS_VINS; i++) {
-            vins[i] = new Vi("Vi nr. " + i, 1000 + i * 100, 10 + i);
+        Vi[] vins = {
+            new Vi("Roura Blanc", 1234, 42),
+            new Vi("Cercium", 535, 30),
+            new Vi("Llum d'Alba Blanc", 1750, 12)
+        };
+        for (int i=0; i<vins.length; i++) {
             System.out.println("Creat" + vins[i]);
         }
 
-        Botiga botiga = new Botiga(QUANTS_VINS - 1);    // no hi cabran tots els vins
-
+        Botiga botiga = new Botiga(vins.length - 1);    // no hi cabran tots els vins
         System.out.println("Afegim els vins creats a la botiga");
-        for (int i=0; i<QUANTS_VINS; i++) {
+        for (int i=0; i<vins.length; i++) {
             Vi resposta = botiga.afegeix(vins[i]);
             System.out.println("En afegir" + vins[i] + "la botiga ens respon " + resposta);
         }
@@ -26,9 +22,8 @@ public class UsaBotiga {
         Vi resposta = botiga.cerca(nomVi);
         System.out.println("En cercar " + nomVi + " botiga ens respon " + resposta);
 
-        nomVi = vins[QUANTS_VINS - 1].getNom();
+        nomVi = vins[vins.length - 1].getNom();
         resposta = botiga.cerca(nomVi);
         System.out.println("En cercar " + nomVi + " botiga ens respon " + resposta);
-
     }
 }
