82b560c580ee84b005bf8113fc6eb6ccbb576ee9
Laura <cf20laura.martin@iesjoandaustria.org>
2022-05-17 20:39:35 +0200

    fet

--- 05_01_vi/Vi.java
+++ 05_01_vi/Vi.java
@@ -1,97 +1,104 @@
 /* 
 *Aquesta classe es una representació d'un Vi.
+*Amb els seus setters i getters.
 */
 
 public class Vi {
     private String nom;
-    private int preu;   
+    private int preu;
     private int estoc;
-    
-    public Vi (String nom, int preu){
+
+    public Vi(String nom, int preu) {
         this(nom, preu, 0);
     }
-    public Vi (String nom, int preu, int estoc){
+
+    public Vi(String nom, int preu, int estoc) {
         this.nom = normalitzaNom(nom);
-        if(estoc < 0){
+        if (estoc < 0) {
             this.estoc = -1;
         } else {
             setEstoc(estoc);
         }
-        if(preu < 0){
+        if (preu < 0) {
             this.preu = -1;
         } else {
             setPreu(preu);
         }
     }
 
-    public String getNom() {  
+    public String getNom() {
         return nom;
-    }    
-    public int getPreu() {  
+    }
+
+    public int getPreu() {
         return preu;
     }
-    public int getEstoc() {  
+
+    public int getEstoc() {
         return estoc;
-    }  
-    
-    // No pot ser negatiu 
-    public void setPreu(int preu) { 
-        if(preu > 0){
+    }
+
+    // No pot ser negatiu
+    public void setPreu(int preu) {
+        if (preu > 0) {
             this.preu = preu;
         }
-    } 
+    }
+
     // No pot ser negatiu, si l'estoc no es vàlid serà 0
     public void setEstoc(int estoc) {
-        if(estoc > 0){
+        if (estoc > 0) {
             this.estoc = estoc;
         }
-    } 
+    }
+
     public void setNom(String nom) {
         this.nom = normalitzaNom(nom);
     }
-    
-    /* Nom no pot ser espais en blanc o buit
+
+    /*
+     * Nom no pot ser espais en blanc o buit
      * No pot tenir espais ni davant ni darrere
-     * No pot tenir més d'un espai entre paraules */     
+     * No pot tenir més d'un espai entre paraules
+     */
     public static String normalitzaNom(String nom) {
 
-        if(nom.isEmpty() || nom.isBlank()){
+        if (nom.isEmpty() || nom.isBlank()) {
             return "NOM NO VÀLID!";
         }
 
         String nombre = nom.trim();
         String nomValid = "";
 
-        for(int i = 0; i < nombre.length(); i++){
-            if(Character.isLetterOrDigit(nombre.charAt(i))){
-                nomValid += nombre.charAt(i); 
-            } else if(Character.isWhitespace(nombre.charAt(i))){    
-                if(!Character.isWhitespace(nombre.charAt(i+1))){
+        for (int i = 0; i < nombre.length(); i++) {
+            if (Character.isLetterOrDigit(nombre.charAt(i))) {
+                nomValid += nombre.charAt(i);
+            } else if (Character.isWhitespace(nombre.charAt(i))) {
+                if (!Character.isWhitespace(nombre.charAt(i + 1))) {
                     nomValid += nombre.charAt(i);
-                } 
+                }
             }
         }
         return nomValid;
     }
 
-    public boolean esValid(){
-        if(nom.equals("NOM NO VÀLID!")){
+    public boolean esValid() {
+        if (nom.equals("NOM NO VÀLID!")) {
             return false;
         }
-        if(estoc == -1){
+        if (estoc == -1) {
             return false;
-        } 
-        if(preu == -1){
+        }
+        if (preu == -1) {
             return false;
         }
 
         return true;
     }
-    
+
     @Override
     public String toString() {
         return String.format("%n    Vi: %s%n    Preu: %d%n    Estoc: %d%n",
-                         nom, preu, estoc);
+                nom, preu, estoc);
     }
 }
-    
